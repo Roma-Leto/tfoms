795c3552163d7da8c4792837f8bb1ba0a0fac494
Лето <imletoroma@gmail.com>
2025-01-31 11:23:45 +0300

    Настройка админ-панели для новой модели пользователя.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+admin.site.register(User, UserAdmin)
