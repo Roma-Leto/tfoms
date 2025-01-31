c2bc2bcebacdc9e1f2ca84c7355d813266505706
Лето <imletoroma@gmail.com>
2025-01-31 11:22:50 +0300

    Создание класса и менеджера кастомного пользователя для доступа к функционалу приложения.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Email field must be set')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username=username, password=password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(unique=True, max_length=30, blank=False, null=False)
+    email = models.EmailField(unique=True)
+    phone_number = models.IntegerField(blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateField()
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+
+    # Какие поля, кроме указанных в USERNAME_FIELD, обязательны для создания суперпользователя
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
