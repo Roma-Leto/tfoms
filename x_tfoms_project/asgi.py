794105b5c62ae5eb092a0b7d4520ef36a6f3b360
Лето <imletoroma@gmail.com>
2025-02-05 12:21:30 +0300

    изменение конфигурации асинхронного сервера

--- x_tfoms_project/asgi.py
+++ x_tfoms_project/asgi.py
@@ -1,16 +1,16 @@
 """
 ASGI config for x_tfoms_project project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
 
+from channels.routing import get_default_application
 from django.core.asgi import get_asgi_application
+
+from x_tfoms_project.wsgi import application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'x_tfoms_project.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+application = get_default_application()
